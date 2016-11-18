ec438b031d4c7e5c59f1f0a1168615fb9236311c
Ryo <kanadajinlee@yahoo.ca>
2016-11-18 19:07:26 +0000

    I made a variable 'trialDB' to mimic var db, before I pass in this new trialDB into the functions below, which will access Mongo

--- server/index.js
+++ server/index.js
@@ -6,13 +6,58 @@ const PORT          = 8080;
 const express       = require("express");
 const bodyParser    = require("body-parser");
 const app           = express();
-
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
+
+
+/******************** MONGO **********************/
+const {MongoClient} = require("mongodb");
+const MONGODB_URI = "mongodb://localhost:27017/tweeter";
+
+MongoClient.connect(MONGODB_URI, (err, db) => {
+  if (err) {
+    console.error(`Failed to connect: ${MONGODB_URI}`);
+    throw err;
+  }
+  // We have a connection to the "tweeter" db, starting here.
+  console.log(`Connected to mongodb: ${MONGODB_URI}`);
+
+  const collection = db.collection('tweets');
+
+  collection.find().toArray(function (err, result) {
+    if (err) {
+      console.log(err);
+    } else if (result.length) {
+      console.log('Found:', result);
+    } else {
+      console.log('No document(s) found with defined "find" criteria!');
+    }
+    console.log("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+
+    var trialDB = {}
+    delete result['_id'];
+    trialDB['tweets'] = result;
+
+    console.log("trialDB ---->", trialDB);
+
+    console.log("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+    console.log("result ---->", result);
+
+    //Close connection
+    // db.close();
+  });
+
+  // const trialDB = db.tweets(`{MONGODB_URI}`).find();
+});
+/******************** MONGO **********************/
+
+
 
 // The in-memory database of tweets. It's a basic object with an array in it.
 const db = require("./lib/in-memory-db");
 
+console.log("db ---->", db);
+console.log("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
 // The `data-helpers` module provides an interface to the database of tweets.
 // This simple interface layer has a big benefit: we could switch out the
 // actual database it uses and see little to no changes elsewhere in the code
